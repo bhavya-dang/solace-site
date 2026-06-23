eaebdc96024dfcc655983c83e0b6d8a7cc09a777
Bhavya Dang <bhavya.dang127@gmail.com>
2026-06-23 22:39:22 +0530

    fixed tailwind render issue during deployment

---
+++ README.md
@@ -0,0 +1,7 @@
+# Solace
+
+A family of violet themes for calm, focused digital spaces.
+
+Get Solace for your favourite tool -> [website link]()
+
+## Preview
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./util/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {
       colors: {
